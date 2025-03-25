6affbd3692fffddab72977c14e6d3ae6565ff443
Arhann00 <143838079+Arhann00@users.noreply.github.com>
2025-03-25 14:53:44 -0600

    Create vehicle.h

---
+++ vehicle.h
@@ -0,0 +1,12 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+// Function prototypes
+char* createVehicle();
+void displayVehicle(char*);
+char** createGarage(int);
+void displayGarage(char**, int);
+char** removeVehicle(char**, int, int);
+void testGarage();
+
+#endif // VEHICLE_H
